d147a858b7c4efe92ae7699ac2d3a05410bc6156
p-x9 <50244599+p-x9@users.noreply.github.com>
2023-07-15 09:23:35 +0900

    Initial Commit

---
+++ ReplayKitRecordExample/ReplayKitRecordExampleApp.swift
@@ -0,0 +1,18 @@
+//
+//  ReplayKitRecordExampleApp.swift
+//  ReplayKitRecordExample
+//
+//  Created by p-x9 on 2023/07/15.
+//  
+//
+
+import SwiftUI
+
+@main
+struct ReplayKitRecordExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ReplayKitRecordExampleUITests/ReplayKitRecordExampleUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  ReplayKitRecordExampleUITestsLaunchTests.swift
+//  ReplayKitRecordExample
+//
+//  Created by p-x9 on 2023/07/15.
+//  
+//
+
+import XCTest
+
+final class ReplayKitRecordExampleUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
